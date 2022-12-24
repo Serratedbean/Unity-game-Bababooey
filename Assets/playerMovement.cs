d70e5ce11a48659e45fd51e2c9d988095091d2df
denisergen <denisergen67@gmail.com>
2022-12-23 18:22:08 -0800

    THE CHARACTER MOVES
    FOR REAL

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -2,19 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
 
     public CharacterController2D controller;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
     // Update is called once per frame
     void Update()
     {
 
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 }
